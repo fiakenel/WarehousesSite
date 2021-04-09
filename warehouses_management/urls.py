eed9cc827a84355f5beceb7cb6d8e362b30ccd6f
fiakenel <maks.karapud52@gmail.com>
2021-04-09 18:11:45 +0300

    Змінив шлях до додатку

--- warehouses_management/urls.py
+++ warehouses_management/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.warehouses_list, name='warehouses_list'),
-    path('wh<int:warehouse_id>', views.wh, name='wh'),
-    path('whnew', views.whnew, name='whnew'),
-    path('wh<int:warehouse_id>/del', views.whdel, name='whdel'),
-    path('wh<int:warehouse_id>/workers', views.workers_list, name='workers_list'),
-    path('wh<int:warehouse_id>/w<int:worker_id>', views.worker, name='worker'),
+    path('warehouses', views.warehouses_list, name='warehouses_list'),
+    path('warehouses/wh<int:warehouse_id>', views.wh, name='wh'),
+    path('warehouses/whnew', views.whnew, name='whnew'),
+    path('warehouses/wh<int:warehouse_id>/del', views.whdel, name='whdel'),
+    path('warehouses/wh<int:warehouse_id>/workers', views.workers_list, name='workers_list'),
+    path('warehouses/wh<int:warehouse_id>/w<int:worker_id>', views.worker, name='worker'),
+    #path('warehouses/wh<int:warehouse_id>/wnew', views.wo
 ]
 
